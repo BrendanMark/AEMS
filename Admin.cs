c827fb77ac1ca02c9272bf632b45b8c49d836ba4
colvinc22 <tud13239@temple.edu>
2015-09-17 14:16:18 -0400

    Added Classes library again

---
+++ Admin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AEMS
+{
+    public class Admin
+    {
+        private int adminID;
+        private string adminFirstName;
+        private string adminLastName;
+        private string email;
+        private string phoneNumb;
+        private bool superAdmin;
+
+        public string AdminFirstName
+        {
+            get { return adminFirstName; }
+            set { adminFirstName = value; }
+        }
+
+        public int AdminID
+        {
+            get { return adminID; }
+            set { adminID = value; }
+        }
+
+        public string AdminLastName
+        {
+            get { return adminLastName; }
+            set {adminLastName = value }
+        }
+        
+    }
+
+ 
+}
